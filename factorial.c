46d94742d0c0334e859b70e69a3df075d969aebc
anandbhavya <abhavya1403@gmail.com>
2022-10-10 19:40:21 +0530

    Initial commit

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum,i;
+    sum=1;
+    for(int i=1;i<=5;i++){
+        sum=sum*i;}
+        
+        printf("fatorial= %d",sum);
+    
+
+    return 0;
+}
---
+++ square_root.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+    int num;
+    printf("enter a number to find the square root",sqrt(num));
+    scanf("%d",&num);
+
+    return 0;
+}
